c0ee9db5b733b77669dacf594b99a521dd79003b
kevin314 <kevsaidhi@gmail.com>
2018-09-01 11:08:57 -0700

    Fixed formatting

--- programnew.cpp
+++ programnew.cpp
@@ -5,14 +5,15 @@ using namespace std;
 int main()
 {
 	string x;
-	while (x!= "exit")
-	{
-		cout << "Enter word: " <<endl;
+	while (x! = "exit") {
+	
+		cout << "Enter word: " << endl;
 		cin >> x;
-	if(x!= "exit")
-		{
-		cout << x <<endl;
-		}
+		
+		if (x != "exit") {
+	
+		cout << x << endl;
+	}
 	}	
 	return 0;
 }
