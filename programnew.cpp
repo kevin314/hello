2cf6b6e067290b75b2bed5ee67343cd7951fbd4e
kevin314 <kevsaidhi@gmail.com>
2018-09-01 11:32:04 -0700

    Spacing

--- programnew.cpp
+++ programnew.cpp
@@ -5,15 +5,13 @@ using namespace std;
 int main()
 {
 	string x;
-	while (x! = "exit") {
-	
+	while (x != "exit") {
 		cout << "Enter word: " << endl;
 		cin >> x;
-		
 		if (x != "exit") {
-	
 			cout << x << endl;
 		}
 	}	
+
 	return 0;
 }
