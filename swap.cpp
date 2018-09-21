6d72dd25b93ddfd3af022166cd77d8c988145360
kevin314 <kevsaidhi@gmail.com>
2018-09-20 20:32:31 -0700

    swap without calling non pointer variables

---
+++ swap.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int swap(int *a, int *b);
+
+int main()
+{
+	int x, y;
+	
+	printf("Enter int x:\n");
+	scanf("%d", &x);
+	printf("Enter int y:\n");
+	scanf("%d", &y);
+
+	int *ptr1 = &x;
+	int *ptr2 = &y;
+
+	swap(&x, &y);
+
+	printf("int x == %d\n", x);
+	printf("int y == %d\n", y);
+	
+	return 0;
+}
+
+int swap(int *a, int *b)
+{
+	
+	int c;
+        c = *a;
+        *a = *b;
+	*b = c;
+
+	return 0;
+}
